75685e88ae4a2715334e702efd313608d40c4e03
user <user@hera.fritz.box>
2013-02-01 11:47:16 +0100

    Box checks for collisions (untested) / getter, constructor implemented

--- src/engine/Box.java
+++ src/engine/Box.java
@@ -1,12 +1,36 @@
 package engine;
 
+import java.util.ArrayList;
+
 public class Box {
 	
 	int fallSpeed;
+	ArrayList<Integer> lastPos;
+	ArrayList<Integer> curPos;
+	int width;
+	int height;
 
-	boolean isInBox(Box box){
+	public Box(){
+		this.fallSpeed = 1;
+		this.width = 100;
+		this.height = 100;
+	}
 	
-		return true;
+	
+	public Box(int fallSpeed, int width, int height){
+		this.fallSpeed = fallSpeed;
+		this.width = width;
+		this.height = height;
+	}
+	
+	boolean isInBox(Box box){
+		
+		if ((this.height > (box.getCurPos(1) + box.getHeight())) && (this.height < (box.getCurPos(1) + box.getHeight())))
+			return true;
+		if ((this.width > (box.getCurPos(0) + box.getWidth())) && (this.width < (box.getCurPos(0) + box.getWidth())))
+			return true;
+		
+		return false;
 	}
 	
 	void setFallSpeed(int speed){
@@ -15,5 +39,29 @@ public class Box {
 	
 	void move(){
 		
+	}
+	
+	/**
+	 * 
+	 * @return last position of box as array (x,y)
+	 */
+	ArrayList<Integer> getLastPos(){
+		return this.lastPos;
+	}
+	
+	/**
+	 * 
+	 * @return current position of box as array (x,y)
+	 */
+	Integer getCurPos(int coordinate){
+		return this.curPos.get(coordinate);
+	}
+	
+	int getWidth(){
+		return this.width;
+	}
+	
+	int getHeight(){
+		return this.height;
 	}
 }
